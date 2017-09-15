f61968aef9b8a89744300d77d5089180c231b424
Михаил Рахманов <m.rakhmanov@hh.ru>
2017-09-15 08:30:48 +0300

    Added template project files

---
+++ TennisBackend/Package.swift
@@ -0,0 +1,20 @@
+import PackageDescription
+
+let package = Package(
+    name: "TennisBackend",
+    targets: [
+        Target(name: "App"),
+        Target(name: "Run", dependencies: ["App"]),
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 2)
+    ],
+    exclude: [
+        "Config",
+        "Database",
+        "Localization",
+        "Public",
+        "Resources",
+    ]
+)
